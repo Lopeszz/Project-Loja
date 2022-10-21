ff9083efc5a35a736b325c5a3a05e74e10241b31
Lopeszz <giovanilopesmcp@gmail.com>
2022-10-21 12:06:20 -0300

    21-10-Up

--- src/Source/Classes/Table/SystemColor.java
+++
@@ -1,9 +0,0 @@
-package Source.Classes.Table;
-
-import java.awt.Color;
-
-public class SystemColor {
-
-    public final static Color MAIN_COLOR_1 = Color.decode("#8f94fb");
-    public final static Color MAIN_COLOR_2 = Color.decode("#4e54c8");
-}
